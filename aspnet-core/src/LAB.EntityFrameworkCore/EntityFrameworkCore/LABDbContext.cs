eb45ce4bd7b6f62b08ca0d3c1af1c03dbba32f1f
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2023-05-08 14:19:33 +0200

    first commit

---
+++ aspnet-core/src/LAB.EntityFrameworkCore/EntityFrameworkCore/LABDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using LAB.Authorization.Roles;
+using LAB.Authorization.Users;
+using LAB.MultiTenancy;
+
+namespace LAB.EntityFrameworkCore
+{
+    public class LABDbContext : AbpZeroDbContext<Tenant, Role, User, LABDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public LABDbContext(DbContextOptions<LABDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
